5fb795da09773ed94363fd33fbe53fca393f124b
Jaroslaw Kardynia <Jaroslaw.Kardynia.ctr@sabre.com>
2020-08-16 18:49:58 +0200

    exercise-02

--- products-utils/src/main/java/org/professionaldevs/products/ProductUtils.java
+++ products-utils/src/main/java/org/professionaldevs/products/ProductUtils.java
@@ -3,6 +3,6 @@ package org.professionaldevs.products;
 public class ProductUtils {
 
     public static String getBarCode(String id){
-        return "mocked-version-01";
+        return "mocked-version-02";
     }
 }
